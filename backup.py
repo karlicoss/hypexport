6b726236dc8063cb49dffb01a1f26e4717cfc306
Dima Gerasimov <karlicoss@gmail.com>
2019-05-08 21:41:15 +0000

    direct import for hyp backup

--- backup.py
+++ backup.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 import json
 import sys
+from pathlib import Path
 
-import hypothesis
+from kython.misc import import_file
+
+hypothesis = import_file(Path(__file__).parent / 'Hypothesis' / 'hypothesis.py', 'hypothesis')
+
 # https://hypothes.is/account/developer
 from hypothesis_secrets import HYPOTHESIS_USER, HYPOTHESIS_TOKEN
 
-h = hypothesis.Hypothesis(username=HYPOTHESIS_USER, token=HYPOTHESIS_TOKEN)
+def main():
+    h = hypothesis.Hypothesis(username=HYPOTHESIS_USER, token=HYPOTHESIS_TOKEN)
+    
+    data = []
+    for a in h.search_all({'user': HYPOTHESIS_USER}):
+        data.append(a)
+    
+    json.dump(data, sys.stdout, indent=2, sort_keys=True, ensure_ascii=False)
 
-data = []
-for a in h.search_all({'user': HYPOTHESIS_USER}):
-    data.append(a)
-
-json.dump(data, sys.stdout, indent=2, sort_keys=True, ensure_ascii=False)
+if __name__ == '__main__':
+    main()
