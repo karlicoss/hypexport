85410d2aa27d86ab50b215fe43fcb14afd407ce3
Dima Gerasimov <karlicoss@gmail.com>
2019-09-23 00:43:53 +0100

    adapt to common Exporter style

---
+++ README.md
@@ -0,0 +1,1 @@
+* TODO use real `export_helper` file
--- hypexport.py
+++ hypexport.py
@@ -1,23 +1,43 @@
 #!/usr/bin/env python3
 import json
-import sys
 from pathlib import Path
 
 from kython.misc import import_file
 
-hypothesis = import_file(Path(__file__).parent / 'Hypothesis' / 'hypothesis.py', 'hypothesis')
+class Exporter:
+    def __init__(self, *args, **kwargs) -> None:
+        # TODO use submodule??
+        hypothesis = import_file(Path(__file__).absolute().parent / 'Hypothesis' / 'hypothesis.py', 'hypothesis')
+        self.api = hypothesis.Hypothesis(*args, **kwargs);
+        self.user = kwargs['username']
 
-# https://hypothes.is/account/developer
-from hypothesis_secrets import HYPOTHESIS_USER, HYPOTHESIS_TOKEN
+    def export_json(self):
+        data = []
+        for a in self.api.search_all({'user': self.user}):
+            data.append(a)
+        return data
+
+
+def get_json(**params):
+    return Exporter(**params).export_json()
+
 
 def main():
-    h = hypothesis.Hypothesis(username=HYPOTHESIS_USER, token=HYPOTHESIS_TOKEN)
-    
-    data = []
-    for a in h.search_all({'user': HYPOTHESIS_USER}):
-        data.append(a)
-    
-    json.dump(data, sys.stdout, indent=2, sort_keys=True, ensure_ascii=False)
+    from export_helper import setup_parser
+    import argparse
+    # TODO pass docs to setup_parser?
+    # https://hypothes.is/account/developer
+    parser = argparse.ArgumentParser('Export/takeout for your personal Hypothesis data')
+    setup_parser(parser=parser, params=['username', 'token'])
+    args = parser.parse_args()
+
+    params = args.params
+    dumper = args.dumper
+
+    j = get_json(**params)
+    js = json.dumps(j, ensure_ascii=False, indent=2, sort_keys=True)
+    dumper(js)
+
 
 if __name__ == '__main__':
     main()
