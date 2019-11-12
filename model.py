a280ae7c1fc34674f9506a38e7a2a48f90036422
Dima Gerasimov <karlicoss@gmail.com>
2019-11-12 23:50:55 +0000

    Make parsing more defensive, rename to Highlight

--- model.py
+++ model.py
@@ -1,20 +1,19 @@
-from typing import NamedTuple, Optional, Sequence, Iterator
+from typing import NamedTuple, Optional, Sequence, Iterator, Dict, Any, Union
 from pathlib import Path
 import json
 from datetime import datetime
 
 Url = str
 
-class Annotation(NamedTuple):
+class Highlight(NamedTuple):
     dt: datetime
-    title: str
-    content: Optional[str] # might be none if for instance we just marked page with tags. not sure if we want to handle it somehow separately
-    link: Url
-    eid: str
-    text: Optional[str]
-    context: Url
-    tags: Sequence[str]
+    page_title: str
+    page_link: Url
+    id: str
     hyp_link: Url
+    highlight: Optional[str] # might be None if for instance we just marked page with tags. not sure if we want to handle it somehow separately
+    annotation: Optional[str] # user's comment
+    tags: Sequence[str]
 
 
 
@@ -35,35 +34,53 @@ class Model:
             annotations = j['annotations']
         yield from annotations
 
-    def iter_annotations(self) -> Iterator[Annotation]:
+    def iter_highlights(self) -> Iterator[Union[Highlight, Exception]]:
         for i in self._iter_raw():
-            dts = i['created']
-            title = ' '.join(i['document']['title'])
-            selectors = i['target'][0].get('selector', None)
-            if selectors is None:
-                # TODO warn?...
-                selectors = []
-            content: Optional[str] = None
-            for s in selectors:
-                if 'exact' in s:
-                    content = s['exact']
-                    break
-            eid = i['id']
-            link = i['uri']
-            # TODO FIXME UTC?
-            dt = datetime.strptime(dts[:-3] + dts[-2:], '%Y-%m-%dT%H:%M:%S.%f%z')
-            txt = i['text']
-            text = None if len(txt.strip()) == 0 else txt
-            context = i['links']['incontext']
-            yield Annotation(
-                dt=dt,
-                title=title,
-                content=content,
-                link=link,
-                eid=eid,
-                text=text,
-                context=context, # TODO FIXME is context used anywhere?
-                tags=tuple(i['tags']),
-                hyp_link=context,
-            )
+            try:
+                yield self._parse_highlight(i)
+            except Exception as e:
+                yield e
+
+    def _parse_highlight(self, i: Dict[str, Any]) -> Highlight:
+        [tg] = i['target'] # hopefully it's always single element?
+        selectors = tg.get('selector', None)
+        if selectors is None:
+            # TODO warn?...
+            selectors = []
+
+        highlights = [s['exact'] for s in selectors if 'exact' in s]
+
+        # TODO warn? never happend though
+        assert len(highlights) <= 1
+
+        if len(highlights) == 0:
+            highlight = None
+        else:
+            [highlight] = highlights
+
+        content: Optional[str] = None
+        for s in selectors:
+            if 'exact' in s:
+                content = s['exact']
+                break
+
+        page_title = ' '.join(i['document']['title'])
+        page_link = i['uri']
+        hid = i['id']
+        # TODO FIXME UTC?
+        dts = i['created']
+        dt = datetime.strptime(dts[:-3] + dts[-2:], '%Y-%m-%dT%H:%M:%S.%f%z')
+        txt = i['text']
+        annotation = None if len(txt.strip()) == 0 else txt
+        context = i['links']['incontext']
+        return Highlight(
+            dt=dt,
+            page_title=page_title,
+            page_link=page_link,
+            id=hid,
+            hyp_link=context,
+            highlight=highlight,
+            annotation=annotation,
+            tags=tuple(i['tags']),
+        )
 
